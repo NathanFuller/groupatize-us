197e3c98b396707dc19853f393469881dfcf24ba
Mark Lindsay <mlindysay@gmail.com>
2018-04-26 22:18:55 -0600

    Fixed bug: Now a user is joined to an event if they rate projects in that event.

--- mainApp/views.py
+++ mainApp/views.py
@@ -400,6 +400,11 @@ def rate_project_ideas(request, event_id):
 	# find the event
 	events = Event.objects.filter(pk=event_id)
 	event = events[0]
+	
+	#Added user to event if not already added
+	user = User.objects.get(pk=request.session.get('user', None))
+	event.participants.add(user)
+	
 
 	# get the project keys associated with this event
 	project_ideas = event.get_project_ideas()
